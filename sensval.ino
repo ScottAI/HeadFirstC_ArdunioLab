9380d7131b64d7c88c484eb7f9ea41813c7462bf
Vijay Purohit <vijay.pu9@gmail.com>
2017-01-30 00:41:09 +0530

    Add files via upload

---
+++ sensval.ino
@@ -0,0 +1,19 @@
+int ledPin = 13;
+int sensorPin = 0;
+int senval = 0;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+ senval = analogRead(sensorPin);
+  digitalWrite(ledPin, HIGH);
+    Serial.print(senval) ;
+}
